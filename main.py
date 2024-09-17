972bd7f394a3cfb7de8c8ab873d371622b18b143
Maciej <maciusngames@gmail.com>
2024-09-17 16:06:51 +0200

    Complete task

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import wikipedia as wk
 import string
 
+
 def display_title():
     print("*** Welcome to 'Word Guessing Game' ***")
 
@@ -10,7 +11,8 @@ def ask_user_for_text():
 
 
 def clean_text(user_text_input):
-    clean_text = user_text_input.translate(str.maketrans('', '', string.punctuation))
+    clean_text = user_text_input.translate(
+        str.maketrans('', '', string.punctuation))
     return clean_text
 
 
@@ -22,7 +24,7 @@ def search_user_input(user_input):
     wk.set_lang("simple")
 
     try:
-        result = wk.summary(user_input, sentences=5)
+        result = wk.summary(user_input, sentences=2)
         return result
     except wk.exceptions.DisambiguationError as e:
         print(f"Disambiguation Error: {e}")
@@ -35,7 +37,6 @@ def search_user_input(user_input):
         return "An unexpected error occurred."
 
 
-## TODO: fix adding blank space on the text
 def remove_every_5_word(text):
     words = text.split()
     removed_words = []
@@ -45,19 +46,39 @@ def remove_every_5_word(text):
     modified_text = " ".join(words)
     return modified_text, removed_words
 
-###TODO: Create  a method to take a user input for guessing the missing words
-##TODO: Use while loop for user to take all required input
-## TODO: Create a new method for checking ccounting correct answer
+# miros function
+def guess_missing_words(removed_words):
+    guessed_words = []
+    for word in removed_words:
+        guess = input(f"Guess the missing word: ")
+        guessed_words.append(guess)
 
-## TODO: Create a method displaying correct & incorrect  annswer. Show result in percentage(Gerald)
+    return guessed_words
+
+
+def display_text_with_blanks(text):
+    print(text[0])
+
+# TODO: Create  a method to take a user input for guessing the missing words
+# TODO: Use while loop for user to take all required input
+# TODO: Create a new method for checking ccounting correct answer
+
+# TODO: Create a method displaying correct & incorrect  annswer. Show result in percentage(Gerald)
+
+
 def main():
     display_title()
     user_text_input = ask_user_for_text()
     clean_user_input = clean_text(user_text_input)
     final_user_input = make_case_insensitive(clean_user_input)
     summary_result = make_case_insensitive(search_user_input(final_user_input))
-    print(remove_every_5_word(summary_result))
-    ## TODO: call a method for getting user input
+    result = remove_every_5_word(summary_result)
+    display_text_with_blanks(result)
+    removed_words = result[1]
+    missed_words = guess_missing_words(removed_words)
+    
+    
+    
 
 if __name__ == "__main__":
     main()
